8cdf78988211c2366f2dc4a3a1266c85334c0cfb
anthonys5s <anthonymsilva27@gmail.com>
2026-08-21 22:25:52 -0600

    Update SQL file to include current timestamp query
    Removed notes and added a query to select the current timestamp.

--- Week 1/HW1.sql
+++ Week 1/HW1.sql
@@ -1,11 +1,4 @@
 select current_database(); 
 select version();
 select 'Hello SQL';
-
-
-
-NOTES
-* You can create a database with the command CREATE DATABASE (database name);
-* Semicolons are used to end a SQL command, its not used ALL the time but its good practice to stick with
-* To create a table its CREATE TABLE and in parenthesis after that each line is formed by a Data Type ending with a semicolon
-* Two examples of data types you might use would be Varchar and Numeric, as varchar is a text column data type with a max of 50 characters. And the numeric type hold the static numbers
+select current_timestamp;
