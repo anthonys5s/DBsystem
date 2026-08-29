026f6e999a56f3baf7bbb8971e95c46dda25279a
anthonys5s <anthonymsilva27@gmail.com>
2026-08-29 12:04:53 -0600

    Create PracticeDB and Video_Games table with data
    Created a new database and table for video games, and inserted initial data.

--- Week 2/Week2 HW.sql
+++ Week 2/Week2 HW.sql
@@ -1,1 +1,19 @@
+create database PracticeDB
 
+create table Video_Games(title varchar (25), genre varchar (25), year_rel integer, publisher varchar (25))
+
+insert into Video_Games (title, genre, year_rel, publisher)
+values ('MK11', 'Fighting Game', 2019, 'NetherRealm Studios'),
+('Fallout 4', 'Action Role-Play', 2015, 'Bethesda'),
+('1941: Counter Attack', 'Shoot em up', 1990, 'Capcom');
+
+ERROR:  syntax error at or near "."
+LINE 6: insert into Video_Games (title, genre,. year_rel, publisher)
+                                              ^ 
+
+SQL state: 42601
+Character: 273
+
+
+--*One new concept would be the CAST data type. Which is used to transform a value from its stored data type to another
+--*A question would be how the interval type is actually useful short of just using the date type
